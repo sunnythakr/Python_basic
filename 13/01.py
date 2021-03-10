4f136d519af121503e8a715a3ec9feacee9a4d60
sunny <sunnysacademy0@gmail.com>
2021-03-10 17:08:18 +0530

    Exception Handling

---
+++ 13/01.py
@@ -0,0 +1,39 @@
+# Exception handling 
+#in any Programming language there are 2 types of errors are possible 
+
+# 1. syntax error 
+# 2. Runtime error 
+
+# 1. syntax Error 
+# x=11
+#  if x==10     #SyntaxError: invalid syntax
+# print("hello")
+
+
+# print"hello"  #SyntaxError: invalid syntax
+
+# 2. Runtime Error 
+
+# print(10/0)  #ZeroDivisionError: division by zero
+
+# print(10/"ten") TypeError: unsupported operand type(s) for /: 'int' and 'str'
+
+# note : exception handling concept applicable for runtime Errors but not for syntax Error 
+
+
+# What is Exception 
+
+# An unwanted and unexpected event that disturbs normal flow of program is called exception 
+
+# example 
+
+    # ZeroDivisionError
+    # TypeError
+    # ValueError
+    # FileNotFoundError
+    # EOFError
+    # SleepingError
+    # TypePuncturatedError
+    # 
+
+
