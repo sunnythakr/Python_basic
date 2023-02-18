b7fce6b9c3dfa62113e210f4292a3a71f5b71a77
sunny <sunnysacademy0@gmail.com>
2023-02-18 17:05:13 +0530

    problem : Tuple First element as the Number and Second as the Square of the Number

--- Dictionary/6.py
+++ Dictionary/6.py
@@ -1,2 +1,3 @@
 # Python Program to Concatenate Two Dictionaries
 
+    
---
+++ Tuple/1.py
@@ -0,0 +1,8 @@
+# Python Program to Create a List of Tuples with the First Element as the Number and Second Element as the Square of the Number
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second Number: "))
+
+
+
+
+
