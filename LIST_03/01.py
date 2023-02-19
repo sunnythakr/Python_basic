6e4db99471644cdad249c309230a2dc287c146d5
sunny <sunnysacademy0@gmail.com>
2023-02-19 10:21:16 +0530

    problem : LIST Find Largest Number in a List

--- LIST_03/01.py
+++ LIST_03/01.py
@@ -1,1 +1,9 @@
 # Python Program to Find Largest Number in a List
+lst = []
+n = int(input("Enter the numbers of element: "))
+for i in range(1, n+1):
+    b = int(input("Enter the numbers: "))
+    lst.append(b)
+lst.sort()
+print("The biggest number is: ")
+print(lst[-1])
