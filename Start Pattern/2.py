76a722471b37c9c8d5f9a9664ece72cbf3fdf1f9
sunny <sunnysacademy0@gmail.com>
2023-03-04 23:13:25 +0530

    Problem: List swap two elements in a list

---
+++ Start Pattern/2.py
@@ -0,0 +1,17 @@
+# Python program to swap two elements in a list
+
+def swapElement(list, pos1, pos2):
+    lst[pos1], lst[pos2] = lst[pos2], lst[pos1]
+    return lst
+lst = []
+n = int(input("Enter the number of element: "))
+for i in range(n):
+    num  = int(input("Enter the element: "))
+    lst.append(num)
+pos1 = int(input("enter pos1 element:"))
+pos2 = int(input("enter pos2 element:"))
+print("new Swap element list is : ")
+print(swapElement(list, pos1-1, pos2-1))
+
+
+
