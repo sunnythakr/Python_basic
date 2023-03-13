3492e17a46001e558178fe6f478d8293652d6ed2
sunny <sunnysacademy0@gmail.com>
2023-03-13 22:58:49 +0530

    Problem : List Remove multiple elements from a list in Python

--- LIST_03/19.py
+++ LIST_03/19.py
@@ -1,4 +1,7 @@
-
-
-
-
+# # Remove multiple elements from a list in Python
+# lst = [int(l) for l in range("List: ").split(",")]
+lst = [2,3,4,5,6,7]
+# b = {4,5} removing multiple item using dict 
+b = 6  # remove single item simple assign value
+lst.remove(b)
+print(lst)
