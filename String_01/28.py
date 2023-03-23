c07d6c333c42d16a39b5d65a2b848cd071ee9955
sunny <sunnysacademy0@gmail.com>
2023-03-24 00:02:29 +0530

    problem : strings even length words in a string

--- String_01/28.py
+++ String_01/28.py
@@ -1,13 +1,8 @@
-# Python program to print even length words in a stringx
-# Python code
-# To print even length words in string
-
-#input string
-n="This is a python language"
-#splitting the words in a given string
-s=n.split(" ")
-for i in s:
-#checking the length of words
-    if len(i)%2==0: 
-	    print(i, end = ' ')
-
+# Python program to print even length words in a string
+str = input("Enter the strings: ")
+lst = str.split()
+d= []
+for i in lst:
+    if len(i) % 2 == 0:
+        d.append(i)
+print(" ".join(d))
