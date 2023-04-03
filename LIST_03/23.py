aa7f8d829632fe5e124ad23bc49f6de8e2f63ad0
sunny <sunnysacademy0@gmail.com>
2023-04-03 23:43:51 +0530

    problem : list find if the given element is present in list

---
+++ LIST_03/23.py
@@ -0,0 +1,13 @@
+# Given an integer array and another integer element. 
+# The task is to find if the given element is present in array or not.
+class solution:
+    def search(self, arr, N, x):
+        for i in range(N):
+            if arr[i] == x:
+                return i
+        return -1
+s = solution()
+arr = [1,2,3,4]
+N = len(arr)
+x = 3
+print(s.search(arr,N,x))
