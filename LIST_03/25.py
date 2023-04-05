a7904484491047dd8d244ecaaafed9f4c5b1027f
sunny <sunnysacademy0@gmail.com>
2023-04-05 23:45:55 +0530

    added comment

--- LIST_03/25.py
+++ LIST_03/25.py
@@ -5,11 +5,13 @@
 # Output: 6
 class solution:
     def findMissing(self,arr,n):
-        common_diff = (arr[-1]-arr[0])//n
+        # calculate common difference by substracting last element and first element in array by floor division
+        common_diff = (arr[-1]-arr[0])//n 
         for i in range(1,n):
             expected = arr[i-1] + common_diff
             if arr[i] != expected:
                 return expected
+        # missing element is not found return none
         return None
 arr = [2, 4, 8, 10, 12, 14]
 n = len(arr)
