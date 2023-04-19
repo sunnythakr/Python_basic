6e5cde9ba1e2ac00a545445f702dc112c5a8fe52
sunny <sunnysacademy0@gmail.com>
2023-04-19 21:30:09 +0530

    problem :List

---
+++ LIST_03/34.py
@@ -0,0 +1,10 @@
+# Given a sorted array A[] of size N, delete all the duplicated elements 
+# from A[]. Modify the array such that if there are X distinct elements in
+#  it then the first X positions of the array should be filled with them in 
+# increasing order and return the number of distinct elements in the array.
+
+# N = 5
+# Array = [2, 2, 2, 2, 2]
+# Output: [2]
+# Explanation: After removing all the duplicates 
+# only one instance of 2 will remain.
