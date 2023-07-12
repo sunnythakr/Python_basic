5f759772738e7955ecd591cbc3682212c0f2e3e4
sunny <sunnysacademy0@gmail.com>
2023-07-12 12:57:05 +0530

    problem : strings reverse strings

--- String_01/28.py
+++ String_01/28.py
@@ -1,1 +1,12 @@
 #Reverse a strings
+# Input:
+# s = Geeks
+# Output: skeeG
+def reverseWord(s):
+    wrd = ""
+    for i in range(len(s)-1,-1,-1):
+        wrd +=s[i]
+    return wrd
+
+s = "Geeks"
+print(reverseWord(s))
