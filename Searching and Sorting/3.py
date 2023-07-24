1d4dfa14fe01161ae8bb566fed9efe44ef41b3a5
sunny <sunnysacademy0@gmail.com>
2023-07-24 16:07:55 +0530

    problem : searching and sorting update program

--- Searching and Sorting/3.py
+++ Searching and Sorting/3.py
@@ -1,5 +1,15 @@
-# arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0]
-# print(arr.count(0))
-def countZeroes(self, arr, n):
-        # code here
+# Find the count of all the 0's.
+# example 1
+def countZeroes(arr, n):
         return arr.count(0)
+
+# example 2
+def countZeroes(arr, n):
+        count = 0
+        for i in range(0,n):
+            if arr[i] == 0:
+                count +=1
+        return count
+
+print(countZeroes([1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0], 12))  # Output: 3
+print(countZeroes([0, 0, 0, 0, 0], 5))                      # Output: 5
