9e5d86f5d1926bc59fe9f2c0d9614fdb52a47df6
sunny <sunnysacademy0@gmail.com>
2023-07-25 11:35:21 +0530

    problem : Searching and sorting update program reduce complexity O(n) to O(log N)

--- Searching and Sorting/8.py
+++ Searching and Sorting/8.py
@@ -1,3 +1,5 @@
+# Example 1
+# time coplexity O(n)
 def floor(arr, n,x):
     i =0
     while(n>0):
@@ -9,3 +11,24 @@ arr = [1,2,8,10,11,12,19]
 n = len(arr)
 x = 5
 print(floor(arr,n,x))
+
+# example 2
+# time complexity  O(log N)
+
+def findFloor( arr, N, X):
+        low, high = 0, N - 1
+        result = -1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            # If arr[mid] is greater than X, search in the left half
+            if arr[mid] > X:
+                high = mid - 1
+
+            # If arr[mid] is less than or equal to X, search in the right half and update the result
+            else:
+                result = mid
+                low = mid + 1
+
+        return result
