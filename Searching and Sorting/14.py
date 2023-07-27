2b16b03c91b6bc1c9c6eb12dff838c2930e1a96f
sunny <sunnysacademy0@gmail.com>
2023-07-27 16:31:47 +0530

    problem : Searching and Sorting update program

--- Searching and Sorting/14.py
+++ Searching and Sorting/14.py
@@ -1,14 +1,24 @@
 def orderedSort(arr,n):
-    odd = []
-    even = []
-    for i in arr:
-        if (i%2!=0):
-            odd.append(i)
-        else:
-            even.append(i)
+
+    # odd = []
+    # even = []
+    # for i in arr:
+    #     if (i%2!=0):
+    #         odd.append(i)
+    #     else:
+    #         even.append(i)
+    
+    # seperate odd even element 
+    odd = [x for x in arr if x%2 != 0]
+    even = [x for x in arr if x%2 == 0]
+
     odd.sort(reverse=True)
     even.sort()
-    return odd + even
+    arr[:len(odd)] = odd
+    arr[len(even):] = even
+    return arr
+
+    # return odd + even
 
 # arr  =[1, 2, 3, 5, 4, 7, 10]
 arr = [0, 4, 5, 3, 7, 2, 1]
