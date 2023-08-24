f7e7eb97e0a408df3b70ccf095f8d9ca861e7868
sunny <sunnysacademy0@gmail.com>
2023-08-24 19:59:32 +0530

    polymorphism

---
+++ OOPS/Encapsulation/18.py
@@ -0,0 +1,4 @@
+# HospitalManagementSystem in Python. This system models a 
+# hospital with patients, doctors, appointments, and medical records.
+#  It aims to demonstrate encapsulation, multiple interactions,
+#  and system management
