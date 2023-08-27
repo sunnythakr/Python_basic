682c19a7ee842b8b69a6100082c63a93b9610af6
sunny <sunnysacademy0@gmail.com>
2023-08-27 15:52:57 +0530

    polymorphism

---
+++ OOPS/Encapsulation/21.py
@@ -0,0 +1,3 @@
+# EcommerceStore simulation in Python. This example models an online store 
+# with products, customers, shopping carts, orders, and payments. It
+#  showcases encapsulation, interactions, and system management 
