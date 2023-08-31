632c07c094ce4f4f2f0ba5bca2dce643e8128472
sunny <sunnysacademy0@gmail.com>
2023-08-31 20:34:08 +0530

    LinkedList

---
+++ LinkedList/11.py
@@ -0,0 +1,8 @@
+# Given a Singly Linked List of size N, delete all alternate nodes of the list.
+# Input:
+# LinkedList: 1->2->3->4->5->6
+# Output: 1->3->5
+# Explanation: Deleting alternate nodes
+# results in the linked list with elements
+# 1->3->5.
+ 
