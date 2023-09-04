a7e9ca9ac934f698764a8c4d25b868019ce658a4
sunny <sunnysacademy0@gmail.com>
2023-09-04 18:05:54 +0530

    LinkedList

--- LinkedList/13.py
+++ LinkedList/13.py
@@ -1,1 +1,23 @@
 # Move Last Element to Front of a Linked List
+class Solution:
+    def moveToFront(self, head):
+        if not head or not head.next:
+            return head  # The list is empty or has only one element, no change needed
+        
+        # Initialize pointers to traverse the list
+        prev = None
+        current = head
+        
+        # Traverse the list to find the last and second-to-last nodes
+        while current.next:
+            prev = current
+            current = current.next
+        
+        # Disconnect the last node from the list
+        prev.next = None
+        
+        # Move the last node to the front
+        current.next = head
+        head = current
+        
+        return head
