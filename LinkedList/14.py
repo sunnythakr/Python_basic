9d736ffa9706e04d9d499f6cfe7f1304a6187d6b
sunny <sunnysacademy0@gmail.com>
2023-09-06 16:58:31 +0530

    LinkedList

--- LinkedList/14.py
+++ LinkedList/14.py
@@ -1,2 +1,44 @@
 # Finding middle element in a linked list
-# #
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+def getMiddle(head):
+    if not head:
+        return None
+
+    slow_ptr = head
+    fast_ptr = head
+
+    while fast_ptr and fast_ptr.next:
+        slow_ptr = slow_ptr.next
+        fast_ptr = fast_ptr.next.next
+
+    return slow_ptr.data
+
+# Example usage:
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.append(4)
+    ll.append(5)
+
+    middle_element = getMiddle(ll.head)
+    print("Middle element:", middle_element)
