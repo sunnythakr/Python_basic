17c6799ca9baf5e674d2b78e5988c88089a595bc
sunny <sunnysacademy0@gmail.com>
2023-09-16 18:12:47 +0530

    Stack

---
+++ STACK/1.py
@@ -0,0 +1,43 @@
+# Stack
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("pop from empty stack")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("peek from empty stack")
+
+    def size(self):
+        return len(self.items)
+
+# Example usage:
+if __name__ == "__main__":
+    stack = Stack()
+
+    print("Is the stack empty?", stack.is_empty())
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+
+    print("Stack size:", stack.size())
+    print("Top element:", stack.peek())
+
+    while not stack.is_empty():
+        print("Popped:", stack.pop())
+
+    print("Is the stack empty?", stack.is_empty())
